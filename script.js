9df7aeef145edd9e7502358f91f652c4d536bfd9
parham24 <54466283+parham24@users.noreply.github.com>
2025-09-28 12:06:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
 // ثبت اولیه Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js');
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker: Registered'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
 }
 
-// ابعاد واقعی عکس نقشه شما (عرض و ارتفاع به پیکسل)
-const mapWidth = 2338;
-const mapHeight = 1700;
+// مختصات مرکز حرم امام رضا (ع)
+const mapCenter = [36.288, 59.616];
+
+// سطح زوم اولیه: عدد کمتر = نمای بازتر از شهر
+// این همان خطی است که تغییر کرده است
+const initialZoom = 13;
 
 // راه‌اندازی نقشه
-const map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -2
-});
+const map = L.map('map').setView(mapCenter, initialZoom);
 
-const bounds = [[0, 0], [-mapHeight, mapWidth]];
-map.fitBounds(bounds);
-
-// قرار دادن عکس نقشه روی صفحه
-L.imageOverlay('map.jpg', bounds).addTo(map);
+// افزودن لایه نقشه از OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
 // خواندن اطلاعات از فایل locations.json و افزودن نشانگرها
 fetch('locations.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(location => {
-            let popupContent = `<b>${location.name}</b><br>${location.category}`;
+            let popupContent = `<b>${location.name}</b><p>${location.category}</p>`;
             L.marker(location.coords)
                 .addTo(map)
                 .bindPopup(popupContent);
         });
     });
+
+// نمایش موقعیت کاربر
+map.locate({
+    watch: true,
+    setView: false, // نقشه به صورت خودکار روی کاربر زوم نکند
+    enableHighAccuracy: true
+});
+
+let userMarker, userCircle;
+
+function onLocationFound(e) {
+    const radius = e.accuracy;
+    const userLatLng = e.latlng;
+
+    if (userMarker) {
+        userMarker.setLatLng(userLatLng);
+        userCircle.setLatLng(userLatLng).setRadius(radius);
+    } else {
+        userMarker = L.marker(userLatLng).addTo(map)
+            .bindPopup(`شما اینجا هستید`).openPopup();
+        userCircle = L.circle(userLatLng, radius, {
+            color: '#136AEC',
+            fillColor: '#136AEC',
+            fillOpacity: 0.15,
+            weight: 2
+        }).addTo(map);
+    }
+}
+
+function onLocationError(e) {
+    alert("امکان دریافت موقعیت مکانی شما وجود ندارد.");
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
