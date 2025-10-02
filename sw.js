c3b49f25c667c271a01fa60c9e18ef0528b5ec7a
parham24 <54466283+parham24@users.noreply.github.com>
2025-10-02 20:18:59 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haram-pwa-cache-v3';
+const CACHE_NAME = 'haram-pwa-cache-v4';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -29,4 +29,5 @@ self.addEventListener('fetch', event => {
     );
 
 });
+
 
